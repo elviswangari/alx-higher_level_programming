83533e3e9e405675c0ed29d6d20afc4106bd6c92
elviswangari <elviswangare@gmail.com>
2023-02-15 00:37:06 +0000

    create datbases

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
---creates a database `hbtn_0c_0` if not exist
-CREATE DATABASE IF NOT EXIST `hbtn_0c_0`;
+-- Creates the database hbtn_0c_0 in my MySQL server.
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
