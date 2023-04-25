c5b118651694fe5ef71dcd99204f0bb42d829992
elvis wangari <elviswangare@gmail.com>
2023-04-25 18:17:24 +0300

    writting to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* a script to write to a file */
+
+const file = process.argv[2];
+const filedata = process.argv[3];
+const fs = require('fs');
+
+const data = fs.writeFileSync(file, filedata);
