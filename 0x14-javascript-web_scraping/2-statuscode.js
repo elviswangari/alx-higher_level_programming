be0f19f80d7d066c548fb090cb99e7c8b1c72ca5
elvis wangari <elviswangare@gmail.com>
2023-04-26 17:33:32 +0300

    request status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* a script that prints the status code */
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (error, response) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:  $(response.statusCode)');
+  }
+});
